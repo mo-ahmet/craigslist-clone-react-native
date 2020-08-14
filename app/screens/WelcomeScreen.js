0846b49a1f1123f1ea55b40e97c2c1f830c665b1
Wingrao <56922415+Wingrao@users.noreply.github.com>
2020-08-14 19:43:37 +0200

    Improved welcome screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import AppButton from "../components/AppButton";
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={10}
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text>Sell What You Don't Need</Text>
+        <Text style={styles.tagLine}>Sell What You Don't Need</Text>
       </View>
-      <View style={styles.logInButton}></View>
-      <View style={styles.registerButton}></View>
+      <View styles={styles.buttonsContainer}>
+        <AppButton tite="login" />
+        <AppButton tite="register" color="secondary" />
+      </View>
     </ImageBackground>
   );
 }
@@ -21,6 +25,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-end",
     alignItems: "center",
+  },
+  buttonsContainer: {
+    padding: 20,
+    width: "100%",
   },
   logoContainer: {
     position: "absolute",
@@ -33,15 +41,10 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 70,
   },
-  logInButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "#fc5c65",
-  },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "#4ecdc4",
+  tagLine: {
+    fontSize: 25,
+    fontWeigh: 600,
+    paddingVertical: 20,
   },
 });
 
