778b25d662264d2528c9eed414d052e3d99281bb
Wingrao <56922415+Wingrao@users.noreply.github.com>
2020-08-19 06:36:04 +0200

    Added validation

--- App.js
+++ App.js
@@ -1,14 +1,6 @@
 import React from "react";
-
-import Screen from "./app/components/Screen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  return (
-    <Screen>
-      <AppPicker icon="apps" placerholder="category" />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
